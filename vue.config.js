aff91e96c5411a8db0ba029dcadb21b7a03234d7
rjswn1031 <78711965+rjswn1031@users.noreply.github.com>
2021-06-23 14:14:35 +0900

    build ver 0.1.8

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    // ? '/pages/'
-    // : '/',
-    publicPath: '/pages/',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/pages/'
+    : '/',
     outputDir: './docs'
 }
